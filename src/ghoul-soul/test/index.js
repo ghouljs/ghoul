ceb4398bb67dcb31c9bcaaa7adb49b9a0c1f0af8
whatwewant <tobewhatwewant@gmail.com>
2017-12-11 19:50:31 +0800

    :sparkles:

---
+++ src/ghoul-soul/test/index.js
@@ -0,0 +1,27 @@
+import expect from 'expect';
+import soul from '../src';
+
+const model = {
+  namespace: 'app',
+  state: 0,
+  reducers: {
+    '+'(state) {
+      return state + 1;
+    },
+  },
+  effects: {
+    async '*+'(action, { dispatch }) {
+      dispatch({ type: 'app@+' });
+    },
+  },
+};
+
+describe('soul', () => {
+  it('normal', () => {
+    const app = soul();
+
+    app.model(model);
+
+    app.start();
+  });
+});
